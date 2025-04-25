758055290b3c5c70f9650f6687b86a00f4822c06
guumkie <rmsdud3811@gmail.com>
2025-04-25 14:44:10 +0900

    basic setting

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php
+    $message = "Welcome to KISIA HOTEL!";
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>KISIA HOTEL</title>
+    </head>
+    <body>
+        <p><?php echo $message; ?></p>
+    </body>
+</html>
