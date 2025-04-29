b0b05521c68fbb3d7b1d6339e326ba6930c3c787
KHC1022 <110967720+KHC1022@users.noreply.github.com>
2025-04-30 01:18:02 +0900

    login

--- src/action/login_action.php
+++ src/action/login_action.php
@@ -5,22 +5,24 @@ include_once '../includes/db_connection.php';
 $id = $_POST['id'];
 $password = $_POST['password'];
 
-$sql = "SELECT user_id, password FROM users WHERE user_id='$id' AND password='$password'";
-$result = $mysqli->query($sql);
+$sql = "SELECT user_id, username, password, username FROM users WHERE user_id='$id' AND password='$password'";
+$result = $conn->query($sql);
 $row = $result->fetch_array(MYSQLI_ASSOC);
 
 if ($row!=null) {
-    $_SESSION['username'] = $row['id'];
-    $_SESSION['name'] = $row['name'];
+    $_SESSION['username'] = $row['user_id'];
+    $_SESSION['name'] = $row['username'];
     echo "<script>
             alert('로그인 성공');
             window.location.href = '../index.php';
-        </script>";
+    </script>";
+    exit;
 }
 else {
     echo "<script>
-            alert('로그인 실패');
+            alert('아이디 또는 패스워드가 틀렸습니다.');
             history.back();
-        </script>";
+    </script>";
+    exit;
 }
 ?>
--- src/includes/db_connection.php
+++ src/includes/db_connection.php
@@ -1,14 +1,11 @@
 <?php
-
 $host = 'mysql';
 $user = 'kisia';
 $password = 'kisia';
-$databaseName = 'kisia_hotel';
+$database = 'kisia_hotel';
 
-$conn = new mysqli($host, $user, $password, $databaseName);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
-    die("DB 연결 실패 : ". $conn->connect_error);
+    die("DB 연결 실패 : " . $conn->connect_error);
 }
-
-?>
